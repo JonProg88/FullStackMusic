4a7d3cd39f6519d84fa7ba4f25bf2b0c9854adae
JonProg88 <alanisJon_Pro@outlook.com>
2025-10-22 14:23:59 -0600

    Asignacion de fullstac music

---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces;
+using Domain.DTOs;
+using Domain.Entities;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repo;
+        public ProductService(IProductRepository repo) => _repo = repo;
+
+
+        public Task<IEnumerable<Product>> GetAllAsync() => _repo.GetAllAsync();
+        public Task<Product?> GetByIdAsync(int id) => _repo.GetByIdAsync(id);
+
+        public async Task<Product> AddAsync(ProductCreateDto dto)
+        {
+            var p = new Product { Name = dto.Name, Price = dto.Price, Stock = dto.Stock };
+            return await _repo.AddAsync(p);
+        }
+
+        public async Task UpdateAsync(int id, ProductCreateDto dto)
+        {
+            var p = await _repo.GetByIdAsync(id);
+            if (p == null) throw new Exception("No encontrado");
+            p.Name = dto.Name;
+            p.Price = dto.Price;
+            p.Stock = dto.Stock;
+            await _repo.UpdateAsync(p);
+        }
+
+        public Task DeleteAsync(int id) => _repo.DeleteAsync(id);
+
+    }
+}
---
+++ Fullstack.api/Program.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// EF Core con SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Inyección de dependencias
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+// CORS (para el front)
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("web", p => p
+        .WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
+// 🔥 ESTA LÍNEA FALTABA
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("web");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Mapea controladores
+app.MapControllers();
+
+app.Run();
